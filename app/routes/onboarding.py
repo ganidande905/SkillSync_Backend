cddd9f3c0611ffe48f62555b2e2e16174ae3f891
ganidande905 <ganidande9@gmail.com>
2025-11-12 21:18:44 +0530

    patch : get method for past project

--- app/routes/onboarding.py
+++ app/routes/onboarding.py
@@ -15,3 +15,12 @@ def create_past_project(
     if not db_project:
         raise HTTPException(status_code=400, detail="Error adding past project (user not found?)")
     return db_project
+@router.get("/pastprojects/{user_email}", response_model=list[UserPastProjectOut])
+def get_past_projects(
+    user_email: str,
+    db: Session = Depends(get_db),
+):
+    db_user = crud.get_user_by_email(db, user_email)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db.query(crud.models.UserPastProject).filter(crud.models.UserPastProject.user_email == user_email).all()
